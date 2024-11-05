6eaa5937c4da889033d377caa75901ab18c1b261
Fran <frannvallone@gmail.com>
2024-11-05 16:24:15 -0300

    Test Suite Persistencia

---
+++ src/testeo_persistencia/PersistenciaTestSuite.java
@@ -0,0 +1,9 @@
+package testeo_persistencia;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PersistenciaTest.class, empresaDTOTest.class})
+public class PersistenciaTestSuite {
+}
