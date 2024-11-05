9e9aa390c2bfb2aa5d288ef0358978363d5216a5
Fran <frannvallone@gmail.com>
2024-11-05 16:22:44 -0300

    PersistenciaTest mas tests

--- src/testeo_persistencia/PersistenciaTest.java
+++ src/testeo_persistencia/PersistenciaTest.java
@@ -1,9 +1,8 @@
 package testeo_persistencia;
 
-import modeloNegocio.Empresa;
+import modeloDatos.Chofer;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import persistencia.EmpresaDTO;
 import persistencia.IPersistencia;
@@ -22,13 +21,14 @@ public class PersistenciaTest {
     EmpresaLlenoFixture1 empresaLlenoFixture1;
     EmpresaVaciaFixture2 empresaVaciaFixture2;
     static String nombreArchivo = "test/data/test.bin";
-    IPersistencia<Serializable> persistencia = new PersistenciaBIN();
+    IPersistencia<Serializable> persistencia;
 
     public PersistenciaTest() {
     }
 
     @Before
     public void setUp() {
+        persistencia = new PersistenciaBIN();
         empresaDTO = new EmpresaDTO();
         empresaLlenoFixture1 = new EmpresaLlenoFixture1();
         empresaVaciaFixture2 = new EmpresaVaciaFixture2();
@@ -39,11 +39,14 @@ public class PersistenciaTest {
     }
 
     @After
-    public void tearDown() {}
+    public void tearDown() {
+        empresaLlenoFixture1.tearDown();
+        empresaVaciaFixture2.tearDown();
+    }
 
     @Test
-    public void testCrearArchivo(){
-        try{
+    public void testCrearArchivo() {
+        try {
             persistencia.abrirOutput(nombreArchivo);
             File archivo = new File(nombreArchivo);
             assertTrue("Debería existir el archivo test.bin", archivo.exists());
@@ -53,34 +56,79 @@ public class PersistenciaTest {
         }
     }
 
-    //verifica la lectoescritura de un diccionario vacío.
-    //la clase Diccionario debe tener sobreescritos los métodos equals y haschode. De otra forma
-    //no puedo comparar dos instancias distintas de la misma clase aunque tengan el mismo estado.
+    //verifica la lectoescritura de una empresa vacía.
     @Test
-    public void testEmpresaVacioArchivo()
-    {
-        try
-        {
+    public void testEmpresaVacioConArchivo() {
+        try {
             empresaVaciaFixture2.setUp();
+            File archivo = new File(nombreArchivo);
 
             persistencia.abrirOutput(nombreArchivo);
+            assertTrue("Debería existir el archivo test.bin", archivo.exists());
             empresaDTO = UtilPersistencia.EmpresaDtoFromEmpresa();
             persistencia.escribir(empresaDTO);
             persistencia.cerrarOutput();
 
             persistencia.abrirInput(nombreArchivo);
-            File archivo = new File(nombreArchivo);
             assertTrue("Debería existir el archivo test.bin", archivo.exists());
 
             EmpresaDTO empresaDTO1 = (EmpresaDTO) persistencia.leer();
-            assertEquals("Los diccionarios deberían ser vacíos", this.empresaDTO, empresaDTO1);
-        }
-        catch (IOException e)
-        {
-            fail("No debería lanzar excepcion: " + e.getMessage());
+            assertEquals("Las empresas deberían ser vacíos", this.empresaDTO.getChoferes().isEmpty(), empresaDTO1.getChoferes().isEmpty());
+            persistencia.cerrarInput();
+        } catch (IOException e) {
+            fail("No debería lanzar excepcion IOException: " + e.getMessage());
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            fail("No debería lanzar excepcion ClassNotFoundExeption: " + e.getMessage());
+
         }
     }
+
+    //verifica la lectoescritura de una empresa con datos.
+    @Test
+    public void testEmpresaConDatosConArchivo() {
+        try {
+            empresaLlenoFixture1.setUp();
+            File archivo = new File(nombreArchivo);
+
+            persistencia.abrirOutput(nombreArchivo);
+            assertTrue("Debería existir el archivo test.bin", archivo.exists());
+            empresaDTO = UtilPersistencia.EmpresaDtoFromEmpresa();
+            persistencia.escribir(empresaDTO);
+            persistencia.cerrarOutput();
+
+            persistencia.abrirInput(nombreArchivo);
+
+            assertTrue("Debería existir el archivo test.bin", archivo.exists());
+
+            EmpresaDTO empresaDTO1 = (EmpresaDTO) persistencia.leer();
+            persistencia.cerrarInput();
+
+            Chofer chofer1 = this.empresaDTO.getChoferes().get(empresaLlenoFixture1.getChofer().getNombre());
+            Chofer chofer2 = empresaDTO1.getChoferes().get(empresaLlenoFixture1.getChofer().getNombre());
+
+            assertEquals("Las empresas deberían ser iguales", chofer1, chofer2);
+        } catch (IOException e) {
+            fail("No debería lanzar excepcion IOException: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            fail("No debería lanzar excepcion ClassNotFoundExeption: " + e.getMessage());
+
+        } catch (Exception e) {
+            fail("No debería lanzar excepcion EXCEPTION: " + e.getMessage());
+
+        }
+    }
+
+    //verifica si se lanza una excepción al intentar leer un archivo inexistente.
+    @Test
+    public void testLecturaSinArchivo() {
+        try {
+            persistencia.abrirInput(nombreArchivo);
+            fail("debería haber lanzado una excepcion de tipo IOException");
+            persistencia.cerrarInput();
+        } catch (IOException e) {
+            //
+        }
+    }
+
 
 }
