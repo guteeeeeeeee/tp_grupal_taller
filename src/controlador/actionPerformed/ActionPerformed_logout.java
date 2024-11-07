7eacf0199386b15c18c5a4daa4f5a627ba3b7fb8
guteeeeeeeee <gutierrezjosemaria01@hotmail.com>
2024-11-07 11:51:13 -0300

    renames

--- src/controlador/actionPerformed/ActionPerformed_logout.java
+++ src/controlador/actionPerformed/ActionPerformed_logout.java
@@ -6,12 +6,16 @@ import static org.mockito.Mockito.when;
 
 import java.awt.event.ActionEvent;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import controlador.Controlador;
+import excepciones.PasswordErroneaException;
+import excepciones.UsuarioNoExisteException;
 import modeloDatos.Moto;
 import modeloNegocio.Empresa;
+import modeloDatos.Administrador;
 import modeloDatos.Chofer;
 import modeloDatos.ChoferTemporario;
 import modeloDatos.Cliente;
@@ -29,14 +33,16 @@ public class ActionPerformed_logout {
 	Vehiculo moto;
 	Pedido pedido;
 	Cliente user;
+	String nombre_usuario;
+	String password;
 	FalsoOptionPane op;
 	
 	@Before
 	public void setUp() throws Exception {
 		this.vista_mock = mock(Ventana.class);
-		Empresa.getInstance().agregarCliente("alfa", "aaa", "alfonso");
-		
-		this.user = (Cliente) Empresa.getInstance().login("alfa","aaa");
+		this.nombre_usuario = "alfa";
+		this.password = "alfa123";
+		Empresa.getInstance().agregarCliente(nombre_usuario, password, "alfonso");
 		
 		this.controlador = new Controlador();
 		this.controlador.setVista(vista_mock);
@@ -45,17 +51,34 @@ public class ActionPerformed_logout {
 	}
 
 	@Test
-	public void test_logout_cliente() { //fijarse
+	public void test_logout_cliente() throws UsuarioNoExisteException, PasswordErroneaException {
+		Cliente log_sistema;
+		Cliente logeado = (Cliente) Empresa.getInstance().login(nombre_usuario, password);
 		ActionEvent mockEvent = mock(ActionEvent.class);
 		when(mockEvent.getActionCommand()).thenReturn("CERRAR_SESION_CLIENTE");
+		log_sistema = (Cliente) Empresa.getInstance().getUsuarioLogeado();
+		assertEquals(logeado.getNombreUsuario(),log_sistema.getNombreUsuario());
 		this.controlador.actionPerformed(mockEvent);
+		log_sistema = (Cliente) Empresa.getInstance().getUsuarioLogeado();
+		assertNull(log_sistema);
 	}
 	
 	@Test
-	public void test_logout_admin() { //fijarse
+	public void test_logout_admin() throws UsuarioNoExisteException, PasswordErroneaException {
+		Administrador admin_logeado = (Administrador) Empresa.getInstance().login("admin", "admin");
 		ActionEvent mockEvent = mock(ActionEvent.class);
+		Administrador logeado = (Administrador) Empresa.getInstance().getUsuarioLogeado();
+		assertEquals(admin_logeado.getNombreUsuario(),logeado.getNombreUsuario());
 		when(mockEvent.getActionCommand()).thenReturn("CERRAR_SESION_ADMIN");
 		this.controlador.actionPerformed(mockEvent);
+		logeado = (Administrador) Empresa.getInstance().getUsuarioLogeado();
+		assertNull(logeado);
+	}
+	
+	@After
+	public void limpiar() {
+		Empresa.getInstance().logout();
+		Empresa.getInstance().getClientes().clear();
 	}
 
 }
